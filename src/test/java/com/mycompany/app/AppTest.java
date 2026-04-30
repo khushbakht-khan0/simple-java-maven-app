ff02178e2d6cdaab6b88f23a46fdad4391294da5
Khushbakht Khan <132307035+khushbakht-khan0@users.noreply.github.com>
2026-04-30 10:40:56 +0500

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello Khushbakht Kesi ho !", app.getMessage());
     }
 }
